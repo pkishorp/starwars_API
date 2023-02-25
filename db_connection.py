54bd7ea9f6212f7a25d57918fcc199a6e4f1a128
pooja <pkishor.pawar92@gmail.com>
2023-02-25 21:16:03 +0530

    done

--- db_connection.py
+++ db_connection.py
@@ -6,18 +6,20 @@ port: 3306
 database: starwarsDB
 """
 
-import pymysql.cursors
+import pymysql
 
 # Connect to the database
-connection = pymysql.connect(host='127.0.0.1',
-                             user='root',
-                             port=3306,
-                             password='Pooja@1992',
-                             database='starwarsDB',
-                             cursorclass=pymysql.cursors.DictCursor)
+connection = pymysql.connect(
+    host="127.0.0.1",
+    user="root",
+    port=3306,
+    password="Pooja@1992",
+    database="starwarsDB",
+    cursorclass=pymysql.cursors.DictCursor,
+)
 
-cursor=connection.cursor()
+cursor = connection.cursor()
 cursor.execute("SHOW DATABASES")
-results=cursor.fetchall()
+results = cursor.fetchall()
 for result in results:
-    print (result)
+    print(result)
